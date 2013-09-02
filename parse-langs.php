23a7640bad7565d149196fdc388014bbc8558f51
Jonathan Reeve <jon.reeve@gmail.com>
2013-09-02 17:40:01 -0400

    language parsing script working now

--- parse-langs.php
+++ parse-langs.php
@@ -23,18 +23,17 @@ $result=dbquery("CREATE TABLE IF NOT EXISTS lang_dict (
 	)") or die("Failed to create table."); 
 
 //make the table Unicode-compatable
-$result=dbquery("ALTER TABLE etym_dict CHARACTER SET utf8 COLLATE utf8_general_ci")
+$result=dbquery("ALTER TABLE lang_dict CHARACTER SET utf8 COLLATE utf8_general_ci")
 	or die("Failed to create table."); 
 
 function parse($line) { 
 		$data=str_getcsv($line); 
 		$lang=$data[0]; 
 		$code=$data[1]; 
-		$query="INSERT INTO etym_dict(language, code) VALUES (\"$lang\",\"$code\")"; 
+		$query="INSERT INTO lang_dict(language, code) VALUES (\"$lang\",\"$code\")"; 
 		$result=dbquery($query)
 			or die ("There was a problem inserting language codes into the database."); 
 	} 
-} 
 
 //open the etymological wordnet for parsing
 $handle = fopen("langs.csv", 'r'); 
