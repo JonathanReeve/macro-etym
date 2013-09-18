a8dfaa1f8e7a7433e138c1e07e9c9fa371d101a7
Jonathan Reeve <jon.reeve@gmail.com>
2013-09-18 16:13:41 -0500

    added derivative forms from etymwn to new database table

--- parse.php
+++ parse.php
@@ -1,8 +1,22 @@
 <?php
 
-error_reporting(E_ALL); 
+//error_reporting(E_ALL); 
 
-echo "Starting. Connecting..."; 
+//fix for flush 
+for($i = 0; $i < 40000; $i++)
+{
+echo ' '; // extra spaces
+} 
+
+function debug_print($text) { 
+	echo $text; 
+	ob_flush(); 
+	flush(); 
+} 
+
+debug_print("This script parses etymwn.tsv, the Etymological Wordnet, into a mySQL database. Don't run this script twice, otherwise it'll create duplicate entries in the database."); 
+
+debug_print("Starting. Connecting..."); 
 
 //setup php for working with Unicode data
 mb_internal_encoding('UTF-8');
@@ -14,8 +28,9 @@ ob_start('mb_output_handler');
 
 include('dblayer.php'); 
 
-echo "Connected to database."; 
+debug_print("Connected to database."); 
 
+debug_print("Creating tables."); 
 //make the table
 $result=dbquery("CREATE TABLE IF NOT EXISTS etym_dict ( 
 	word VARCHAR(30),
@@ -29,11 +44,27 @@ or die("Failed to create table.");
 $result=dbquery("ALTER TABLE etym_dict CHARACTER SET utf8 COLLATE utf8_general_ci")
 	or die("Failed to create table."); 
 
+$result=dbquery("CREATE TABLE IF NOT EXISTS derivations ( 
+	word VARCHAR(30),
+	word_lang CHAR(3),
+	parent_word VARCHAR(30),
+	parent_lang CHAR(3)
+	)") 
+or die("Failed to create derivations table."); 
+
+//make the table Unicode-compatable
+$result=dbquery("ALTER TABLE derivations CHARACTER SET utf8 COLLATE utf8_general_ci")
+	or die("Failed to make derivations table UTF-8 compatible."); 
+
+debug_print("Created tables. "); 
+
+debug_print("Processing stuff. "); 
+
 function parse($line) { 
 	$pattern = "/.*\trel:etymology\t.*\n/"; 
 	if (preg_match_all($pattern, $line, $matches)) {
 		$match = $matches[0][0]; //the matched string appears at 0:0 in the array for some reason
-//		echo "matches: ".$match; 
+		//echo "matches: ".$match; 
 		$match_pieces = explode("\t", $match); 
 		$word_pieces = explode(": ",$match_pieces[0]); 
 		$parent_pieces = explode(": ",$match_pieces[2]); 
@@ -41,14 +72,37 @@ function parse($line) {
 		$word = addslashes($word_pieces[1]); 
 		$parent_lang = addslashes($parent_pieces[0]);
 		$parent_word = addslashes($parent_pieces[1]); 
-//		echo "<p>Word: $word</p>"; 
-//		echo "<p>Word lang: $word_lang</p>"; 
-//		echo "<p>Parent lang: $parent_lang</p>"; 
-//		echo "<p>Parent Word: $parent_word</p>"; 
+		//echo "<p>Word: $word</p>"; 
+		//echo "<p>Word lang: $word_lang</p>"; 
+		//echo "<p>Parent lang: $parent_lang</p>"; 
+		//echo "<p>Parent Word: $parent_word</p>"; 
 
 		$query="INSERT INTO etym_dict(word, word_lang,parent_word, parent_lang) VALUES (\"$word\",\"$word_lang\", \"$parent_word\", \"$parent_lang\")"; 
 		$result=dbquery($query)
 			or die ("There was a problem inserting stuff into the etymological dictionary database."); 
+	} 
+	$derivations_pattern = "/.*\trel:is_derived_from\t.*\n/"; 
+	if (preg_match_all($derivations_pattern, $line, $matches)) { 
+		$match = $matches[0][0]; //the matched string appears at 0:0 in the array for some reason
+	//	echo "matches: ".$match; 
+		$match_pieces = explode("\t", $match); 
+		$word_pieces = explode(": ",$match_pieces[0]); 
+		$parent_pieces = explode(": ",$match_pieces[2]); 
+		$word_lang = addslashes($word_pieces[0]); 
+		$word = addslashes($word_pieces[1]); 
+		$parent_lang = addslashes($parent_pieces[0]);
+		$parent_word = addslashes($parent_pieces[1]); 
+	//	echo "<p>Word: $word</p>"; 
+	//	echo "<p>Word lang: $word_lang</p>"; 
+	//	echo "<p>Parent lang: $parent_lang</p>"; 
+	//	echo "<p>Parent Word: $parent_word</p>"; 
+		// debug_print(". "); 
+		$word_numwords = count(split(" ",$word)); 
+		if($word_numwords==1) { //add only single words to database
+			$query="INSERT INTO derivations(word, word_lang,parent_word, parent_lang) VALUES (\"$word\",\"$word_lang\", \"$parent_word\", \"$parent_lang\")"; 
+			$result=dbquery($query)
+				or die ("There was a problem inserting stuff into the etymological dictionary database."); 
+		} 
 	} 
 } 
 
@@ -64,7 +118,7 @@ if ($handle) {
    fclose($handle);
 }
 
-echo "Closing database connection."; 
+debug_print("Closing database connection."); 
 dbclose($dbc); 
 
 ?> 
