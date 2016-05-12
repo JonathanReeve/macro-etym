78d7685bef8c4ea5c87a63e369a4f761b4253866
Jonathan Reeve <jon.reeve@gmail.com>
2016-05-12 14:17:56 -0400

    reconfiguring project for upload to pypi

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ This is a rewrite of [The Macro-Etymological Analyzer](http://jonreeve.com/etym)
  * It is now possible to analyze and compare multiple texts at a time. 
  * Users can filter for only those language families they care about. 
 
-##Requirements
-This Python script requires the modules csv, collections, nltk, pycountry, pandas, matplotlib, click, codecs, and logging. It runs on Python 3. 
+##Installation
+This Python script requires the modules nltk, pycountry, pandas, matplotlib, and click. It runs on Python 3. 
+
+git clone https://github.com/JonathanReeve/macro-etym
+pip install .
 
 ##Usage
 
-    python macro-etym.py moby-dick.txt pride-and-prejudice.txt
+    macroetym moby-dick.txt pride-and-prejudice.txt
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+  name = 'macroetym',
+  packages = ['macroetym'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A tool for macro-etymological textual analysis.',
+  author = 'Jonathan Reeve',
+  author_email = 'jon.reeve@gmail.com',
+  url = 'https://github.com/JonathanReeve/macro-etym', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  install_requires = ['Click', 'nltk', 'pycountry', 'pandas',
+                      'matplotlib'],
+  keywords = ['nlp', 'text-analysis', 'etymology'], # arbitrary keywords
+  classifiers = [],
+  entry_points='''
+      [console_scripts]
+      macroetym = macroetym.main:cli
+  ''',
+)
