a6af7b441273270fb71f0cbc05465e73c50f364e
Jonathan Reeve <jon.reeve@gmail.com>
2016-05-13 16:31:52 -0400

    fix debugging messages

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from distutils.core import setup
 if sys.version_info[0] < 3:
     version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
     sys.exit("""
-    Sorry! Your Python version is %s, but macro-etym requires at least 
+    Sorry! Your Python version is %s, but macro-etym requires at least
     Python 3. Please upgrade your Python installation, or try using pip3
     instead of pip.""" % version)
-
-exit() 
 
 setup(
   name = 'macroetym',
@@ -17,17 +15,15 @@ setup(
   description = 'A tool for macro-etymological textual analysis.',
   author = 'Jonathan Reeve',
   author_email = 'jon.reeve@gmail.com',
-  url = 'https://github.com/JonathanReeve/macro-etym', 
-  
+  url = 'https://github.com/JonathanReeve/macro-etym',
   download_url = 'https://github.com/JonathanReeve/macro-etym/tarball/0.1', # FIXME: make a git tag and confirm that this link works
   install_requires = ['Click', 'nltk', 'pycountry', 'pandas',
                       'matplotlib'],
   include_package_data = True,
-  package_data = {'macroetym': ['etymwm-smaller.tsv']}, 
-  keywords = ['nlp', 'text-analysis', 'etymology'], 
+  package_data = {'macroetym': ['etymwm-smaller.tsv']},
+  keywords = ['nlp', 'text-analysis', 'etymology'],
   classifiers = [],
   entry_points='''
-      [console_scripts]
-      macroetym = macroetym.main:cli
-  ''',
+  [console_scripts]
+  macroetym = macroetym.main:cli''',
 )
