13f86a09cbdb3c8153c2ce0d5c53320f43de61bf
Jonathan Reeve <jon.reeve@gmail.com>
2016-05-13 15:08:51 -0400

    check for python version before installing

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+import sys
 from distutils.core import setup
+
+if sys.version_info[0] < 3:
+    version = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+    sys.exit("""
+    Sorry! Your Python version is %s, but macro-etym requires at least 
+    Python 3. Please upgrade your Python installation, or try using pip3
+    instead of pip.""" % version)
+
+exit() 
+
 setup(
   name = 'macroetym',
   packages = ['macroetym'], # this must be the same as the name above
